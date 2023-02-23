5713ad243e7357f32b08fcb916e515396f75b9ae
Andrea <afarinon93@gmail.com>
2023-02-23 20:18:55 -0300

    probando

--- java.js
+++ java.js
@@ -17,6 +17,13 @@ cancelar.addEventListener ("click", function() {
     document.getElementById('formulario').style.display= 'none';
   });
 
+
+
+  var mio = document.getElementById("mio");
+ 
+
+
+
   function validarform() {
     let x = document.forms["formulario"]["input"].value;
     if (x == "") {
@@ -26,64 +33,8 @@ cancelar.addEventListener ("click", function() {
   }
 
 
- document.getElementsByTagName("input").oninput = function() {myFunction()};
-
-  function myFunction() {
-    document.getElementsByTagName("label").innerHTML = "The value of the input field was changed.";
-  }
-
-  /*
-
-const v = document.querySelectorAll(".forminput");
-for (let i = 0; i < v.length; i++) {
-    v[i].addEventListener('input', modificar);
-}
-
-function modificar() {
-    document.querySelectorAll(".formlabel").style.display='none';
-}
 
 
 
 
-
-/*{modificar()};
-
-  function modificar() {
-    x.getElementsByTagName("formlabel").innerHTML = "The value of the input field was changed.";
-  }
-
-
-/*
-function fijar () {
-    var value = document.getElementsByClassName('forminput').value;
-    if (value.length>=3) {
-       return false;
-    }
-    else {
-        return true;
-    }
-}
-    fijar( )
-            
-    
-    
-    
-    /*
-    
-    labels.style.setProperty ("margin-top", "-110px");
-        } else {
-            labels.style.setProperty ("margin-top", "0px");
-        }
-    })
-
-
-
-/*
-
-fijarlabel ()
-
-
-
-
-ul.style.setProperty("background-color", "#6ab+*/
+  
