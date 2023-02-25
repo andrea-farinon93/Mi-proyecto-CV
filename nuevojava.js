d02d4bdcd13957688650505c38a09535a1acf88e
Andrea <afarinon93@gmail.com>
2023-02-25 05:19:58 -0300

    validacion formulario

--- nuevojava.js
+++ nuevojava.js
@@ -22,54 +22,74 @@ const formulario=document.getElementById('formulario');
 const inputs=document.querySelectorAll('#formulario input')
 const expresiones= {
   nombre:/^[a-zA-ZÁ-ÿ\s]{1,15}$/,
-  correo: /^ ( [ A - Za - z0 - 9 _\ - \. ] ) + \@ ( [ A - Za - z0 - 9 _\ - \. ] ) + \. ([ A - Za - z ] { 2 , 4 } ) $ /,
-  telefono: /^\d{7,14}$/,
-  mensaje: /^[a-zA-ZÁ-ÿ\s]{1,100}$/,
+  correo: /^[^]{1,100}$/,
+  telefono: /^[a-zA-ZÁ-ÿ\s]{1,15}$/,
+  mensaje: /^[^]{1,100}$/,
 }
 
+/*
+const campos {
+  nombre= false,
+  email= false,
+  telefono= false,
+  mensaje= false,
+}
+*/
 
 const validarformulario=(e) => {
  switch (e.target.name) {
     case "nombre":
-      if (expresiones.nombre.test(e.target.value)) {
-        document.getElementById('grupo_nombre').classList.remove('grupo_incorrecto');
-        document.getElementById('grupo_nombre').classList.add('grupo_correcto');
-        document.querySelector('#grupo_nombre .advertencia').classList.remove('advertencia_activo');
-        document.querySelector('#grupo_nombre i').classList.add('validacion_activo');
-        document.querySelector('#grupo_nombre i').classList.remove('validacion');
-
-
-
-      } else {
-        document.getElementById('grupo_nombre').classList.add('grupo_incorrecto');
-        document.getElementById('grupo_nombre').classList.remove('grupo_correcto');
-        document.querySelector('#grupo_nombre .advertencia').classList.add('advertencia_activo');
-        document.querySelector('#grupo_nombre i').classList.remove('validacion_activo');
-        document.querySelector('#grupo_nombre i').classList.add('validacion');
-
-      }
-
+      validarcampo (expresiones.nombre, e.target, 'nombre');
       break;
     case "email":
-      
-
+      validarcampo (expresiones.correo, e.target, 'email');
       break;
-    case "tel":
-      
-
+    case "telefono":
+      validarcampo (expresiones.telefono, e.target, 'telefono');
       break;
     case "mensaje":
-      
-
+      validarcampo (expresiones.mensaje, e.target, 'mensaje');
       break;
- }
-}
+    }
+    }
+    
+    const validarcampo = (expresion, input, campo) => {
+    if (expresion.test(input.value)) {
+    document.getElementById(`grupo_${campo}`).classList.remove('grupo_incorrecto');
+    document.getElementById(`grupo_${campo}`).classList.add('grupo_correcto');
+    document.querySelector(`#grupo_${campo} .advertencia`).classList.remove('advertencia_activo');
+    document.querySelector(`#grupo_${campo} i`).classList.add('validacion_activo','material-symbols-outlined');
+    document.querySelector(`#grupo_${campo} i`).classList.remove('validacion');
+    /*campos[campo] = true;*/
+    } else {
+    document.getElementById(`grupo_${campo}`).classList.add('grupo_incorrecto');
+    document.getElementById(`grupo_${campo}`).classList.remove('grupo_correcto');
+    document.querySelector(`#grupo_${campo} .advertencia`).classList.add('advertencia_activo');
+    document.querySelector(`#grupo_${campo} i`).classList.remove('validacion_activo', 'material-symbols-outlined');
+    document.querySelector(`#grupo_${campo} i`).classList.add('validacion');
+   /* campos[campo] = false;*/
+    } }
 
 inputs.forEach((input) => {
   input.addEventListener('keyup', validarformulario);
   input.addEventListener('blur', validarformulario);
 });
 
+/*
 formulario.addEventListener('submit', (e) => {
   e.preventDefault(); 
+if (campos.nombre && campos.email && campos.telefono && campos.mensaje) {
+formulario.reset (); 
+
+document.querySelector('#mensaje p').classList.add('.exito_activo'); 
+setTimeout (() => { document.querySelector('#mensaje p').classList.add('.exito_activo'); }, 5000);
+
+document.querySelectorAll('grupo_correcto').forEach ((icono) => {
+icono.classList.remove('grupo_correcto');
 });
+
+else {
+  document.getElementById('error').classList.add(exclamacion_activo)
+}
+
+}});*/
